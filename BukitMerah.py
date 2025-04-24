b923b2cf819c9d6a232f7ba786e2dd20a620d8b9
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2025-04-24 12:42:08 +0800

    Update BukitMerah.py

--- BukitMerah.py
+++ BukitMerah.py
@@ -79,12 +79,14 @@ if location == "Bukit Merah":
         st.markdown("### <div style='text-align: center;'>Jumlah keseluruhan respon kepada lokasi</div>", unsafe_allow_html=True)
         center_content(lambda: st.dataframe(data['Lokasi_strategik'].value_counts().to_frame(name='Jumlah')))
 
+    
     if st.checkbox("📍 Kekurangan Pengunjung"):
         plot_grouped_line('Kekurangan_pengunjung', 'Umur vs  Keperluan Perpustakaan')
         st.markdown("### <div style='text-align: center;'>Bilangan responden ikut umur dan keperluan perpustakaan</div>", unsafe_allow_html=True)
         center_content(lambda: st.dataframe(data.groupby('Umur')['Keperluan_perpustakaan'].value_counts().unstack(fill_value=0)))
         st.markdown("### <div style='text-align: center;'>Jumlah keseluruhan respon kepada keperluan perpustakaan</div>", unsafe_allow_html=True)
         center_content(lambda: st.dataframe(data['Keperluan_perpustakaan'].value_counts().to_frame(name='Jumlah')))
+
     
     if st.checkboxf("📍 Keperluan Perpustakaan"):
         plot_grouped_line('Keperluan_perpustakaan', 'Bilangan Responden vs Kekurangan Pengunjung')
@@ -93,6 +95,7 @@ if location == "Bukit Merah":
         st.markdown("### <div style='text-align: center;'>Jumlah keseluruhan respon keperluan perpustakaan</div>", unsafe_allow_html=True)
         center_content(lambda: st.dataframe(data['Keperluan_perpustakaan'].value_counts().to_frame(name='Jumlah')))
 
+    
     if st.checkboxf("📍 Operasi perpustakaan"):
         plot_grouped_line('Kekurangan_pengunjung', 'Bilangan Responden vs Pengoperasian')
         st.markdown("### <div style='text-align: center;'>Bilangan Responden vs Pengoperasian</div>", unsafe_allow_html=True)
@@ -100,6 +103,7 @@ if location == "Bukit Merah":
         st.markdown("### <div style='text-align: center;'>Jumlah keseluruhan respon pengoperasian</div>", unsafe_allow_html=True)
         center_content(lambda: st.dataframe(data['Operasi_perpustakaan'].value_counts().to_frame(name='Jumlah')))
 
+    
     if st.checkboxf("📍 Jarak Lokasi"):
         plot_grouped_bar('Jarak_lokasi', 'Histogram: Bilangan Responden vs Jarak lokasi')
         st.markdown("### <div style='text-align: center;'>Bilangan Responden vs Jarak Lokasi</div>", unsafe_allow_html=True)
