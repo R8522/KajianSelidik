9f4e756c44eec6e18a9cc706df73ffb9c120980b
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2025-04-24 08:39:38 +0800

    Update BukitMerah.py

--- BukitMerah.py
+++ BukitMerah.py
@@ -32,19 +32,19 @@ def plot_grouped_line(column, title):
     ax.set_ylabel('Bilangan Responden')
     ax.set_title(title)
     ax.legend(title=column)
-    ax.grid(True, linestyle='--', alpha=0.6)
+    ax.grid(True, linestyle='--', alpha=0.4)
     plt.xticks(rotation=45)
     st.pyplot(fig)
 
 def plot_grouped_bar(column, title):
     grouped = data.groupby(['Umur', column]).size().unstack(fill_value=0)
     fig, ax = plt.subplots(figsize=(8, 6))
-    grouped.plot(kind='bar', ax=ax, width=0.8)
+    grouped.plot(kind='bar', ax=ax, width=0.5)
     ax.set_xlabel('Kumpulan Umur')
     ax.set_ylabel('Bilangan Responden')
     ax.set_title(title)
     ax.legend(title=column)
-    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    ax.grid(axis='y', linestyle='--', alpha=0.4)
     plt.xticks(rotation=45, ha='right')
     st.pyplot(fig)
 
