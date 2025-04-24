b8cdf9aecfccf1f4629421c8008c8369d8e21cc5
Rabiatul Adawiah <93849400+R8522@users.noreply.github.com>
2025-04-24 11:19:23 +0800

    Update BukitMerah.py

--- BukitMerah.py
+++ BukitMerah.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Analisis Perpustakaan", layout="wide")
 
 # --- Tajuk Besar ---
 st.markdown("<h1 style='text-align: center;'>📚 Analisis Penutupan Perpustakaan Bukit Merah</h1>", unsafe_allow_html=True)
-
+try:
 # --- Load CSV ---
 data = pd.read_csv('BukitMerah.csv')
 
